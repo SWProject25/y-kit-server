b643075c0e839f5ce04669aa02fd7fe88f906d6f
eeun1225 <eeun9108@gmail.com>
2025-10-02 10:32:35 +0900

    feat: Initial commit

---
+++ src/main/java/com/twojz/y_kit/YKitApplication.java
@@ -0,0 +1,13 @@
+package com.twojz.y_kit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YKitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YKitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twojz/y_kit/YKitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twojz.y_kit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YKitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
