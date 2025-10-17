c0683913f2b4a1a31df8150e736166990d08b5a4
eeun1225 <eeun9108@gmail.com>
2025-10-17 11:13:08 +0900

    style: 불필요한 중괄호 및 import문 제거 (#7)

--- src/main/java/com/twojz/y_kit/policy/service/PolicyService.java
+++ src/main/java/com/twojz/y_kit/policy/service/PolicyService.java
@@ -2,7 +2,6 @@ package com.twojz.y_kit.policy.service;
 
 import com.twojz.y_kit.external.policy.client.YouthPolicyClient;
 import com.twojz.y_kit.external.policy.dto.YouthPolicy;
-import com.twojz.y_kit.external.policy.dto.YouthPolicyResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
