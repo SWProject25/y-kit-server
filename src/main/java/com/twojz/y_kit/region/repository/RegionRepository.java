982dabf279a8c0e8e395bc1780aa2383aecf7d65
eeun1225 <eeun9108@gmail.com>
2025-10-21 15:05:14 +0900

    feat: Region Entity 및 Repository 생성 (#9)

---
+++ src/main/java/com/twojz/y_kit/region/repository/RegionRepository.java
@@ -0,0 +1,7 @@
+package com.twojz.y_kit.region.repository;
+
+import com.twojz.y_kit.region.entity.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegionRepository extends JpaRepository<Region, String> {
+}
