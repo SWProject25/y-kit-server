bfb34a8f094dbfe66f05836b2a77baf5fbff4dd4
eeun1225 <eeun9108@gmail.com>
2025-10-21 15:07:40 +0900

    feat: VWorldApiEndpoint 상수 추가 (#9)

---
+++ src/main/java/com/twojz/y_kit/external/vworld/dto/VWorldApiEndpoint.java
@@ -0,0 +1,10 @@
+package com.twojz.y_kit.external.vworld.dto;
+
+public class VWorldApiEndpoint {
+    public static final String ADM_CODE_LIST_PATH = "/admCodeList";
+    public static final String ADM_SI_LIST_PATH = "/admSiList";
+    public static final String ADM_DONG_LIST_PATH = "/admDongList";
+    public static final String ADM_REE_LIST_PATH = "/admReeList";
+
+    private VWorldApiEndpoint() { }
+}
