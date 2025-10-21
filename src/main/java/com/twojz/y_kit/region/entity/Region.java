7cdec7233bc8ad2e37de9e678935f3f31b7a586c
eeun1225 <eeun9108@gmail.com>
2025-10-21 15:05:14 +0900

    feat: Region Entity 및 Repository 생성 (#9)

---
+++ src/main/java/com/twojz/y_kit/region/entity/Region.java
@@ -0,0 +1,48 @@
+package com.twojz.y_kit.region.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Region {
+    @Id
+    private String code;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private RegionLevel level;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_code")
+    private Region parent;
+
+    @OneToMany(mappedBy = "parent")
+    private final List<Region> children = new ArrayList<>();
+
+    @Builder
+    public Region(String name, String code, RegionLevel level, Region parent) {
+        this.name = name;
+        this.code = code;
+        this.level = level;
+        this.parent = parent;
+    }
+}
