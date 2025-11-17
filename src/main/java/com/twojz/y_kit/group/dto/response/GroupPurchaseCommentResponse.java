49f69e45e3db0457a6e3119b503269d92a294639
eeun1225 <eeun9108@gmail.com>
2025-11-17 14:30:32 +0900

    refactor: GroupPurchaseCommentResponse Builder 패턴 추가 (#38)

--- src/main/java/com/twojz/y_kit/group/dto/response/GroupPurchaseCommentResponse.java
+++ src/main/java/com/twojz/y_kit/group/dto/response/GroupPurchaseCommentResponse.java
@@ -2,9 +2,11 @@ package com.twojz.y_kit.group.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class GroupPurchaseCommentResponse {
     @Schema(description = "댓글 ID", example = "1")
     private Long id;
@@ -22,12 +24,12 @@ public class GroupPurchaseCommentResponse {
     private LocalDateTime createdAt;
 
     public static GroupPurchaseCommentResponse from(com.twojz.y_kit.group.domain.entity.GroupPurchaseCommentEntity comment) {
-        GroupPurchaseCommentResponse res = new GroupPurchaseCommentResponse();
-        res.id = comment.getId();
-        res.userId = comment.getUser().getId();
-        res.username = comment.getUser().getName();
-        res.content = comment.getContent();
-        res.createdAt = comment.getCreatedAt();
-        return res;
+        return GroupPurchaseCommentResponse.builder()
+                .id(comment.getId())
+                .userId(comment.getUser().getId())
+                .username(comment.getUser().getName())
+                .content(comment.getContent())
+                .createdAt(comment.getCreatedAt())
+                .build();
     }
 }
