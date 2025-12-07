4b4e3542a2c8871814fffdfc7138b7061b327d41
eeun1225 <eeun9108@gmail.com>
2025-12-08 08:40:59 +0900

    fix: 프로필 입력 완성 알림 전송 조건 추가

--- src/main/java/com/twojz/y_kit/user/controller/UserController.java
+++ src/main/java/com/twojz/y_kit/user/controller/UserController.java
@@ -146,10 +146,12 @@ public class UserController {
 
         UserEntity user = userFindService.findUser(userId);
 
-        try {
-            userNotificationService.sendProfileCompletedNotification(user);
-        } catch (Exception e) {
-            log.warn("프로필 완료 알림 전송 실패 - userId: {}", user.getId(), e);
+        if(user.getProfileStatus() == ProfileStatus.COMPLETED) {
+            try {
+                userNotificationService.sendProfileCompletedNotification(user);
+            } catch (Exception e) {
+                log.warn("프로필 완료 알림 전송 실패 - userId: {}", user.getId(), e);
+            }
         }
 
         return ResponseEntity.ok().build();
